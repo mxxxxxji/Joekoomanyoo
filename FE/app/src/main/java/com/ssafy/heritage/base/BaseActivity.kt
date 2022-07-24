d31438580650bdc522fafe3a8e04c94dcdde9711
DESKTOP-C5AALBF\enddl <aud0725_123@naver.com>
2022-07-25 00:17:07 +0900

    [][S07P12D102-22] UPDATE : navigation 연결

--- FE/app/src/main/java/com/ssafy/heritage/base/BaseActivity.kt
+++ FE/app/src/main/java/com/ssafy/heritage/base/BaseActivity.kt
@@ -2,10 +2,27 @@ package com.ssafy.heritage.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes val layoutResId: Int) : AppCompatActivity() {
+
+    protected lateinit var binding: T
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+        binding.lifecycleOwner = this
+        init()
     }
+
+    override fun onPause() {
+        super.onPause()
+        overridePendingTransition(0, 0)
+    }
+
+    protected abstract fun init()
 }
--- FE/app/src/main/java/com/ssafy/heritage/base/BaseFragment.kt
+++ FE/app/src/main/java/com/ssafy/heritage/base/BaseFragment.kt
@@ -1,60 +1,53 @@
 package com.ssafy.heritage.base
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.ssafy.heritage.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BaseFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BaseFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+abstract class BaseFragment<T: ViewDataBinding>(
+    @LayoutRes val layoutResId: Int
+) : Fragment() {
+
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+    // private lateinit var loadingDialog: LoadingDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false)
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BaseFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BaseFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        init()
+    }
+
+    protected abstract fun init()
+
+    // protected fun loadingStart(){}
+
+    // protected fun loadingEnd(){}
+
+    override fun onDestroy() {
+        super.onDestroy()
+    //    loadingEnd()
+        _binding = null
     }
 }
