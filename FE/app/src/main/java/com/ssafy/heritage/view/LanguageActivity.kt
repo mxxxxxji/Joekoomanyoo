5cfb48d1ce56e4a405a061bf6649e3fe296fe78f
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-07-24 18:45:11 +0900

    [S07P12D102-24] REFACTOR : 화면 생성
    모든 페이지 이름 정함
    모든 페이지 파일 생성

---
+++ FE/app/src/main/java/com/ssafy/heritage/view/LanguageActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.heritage.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.heritage.R
+
+class LanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+    }
+}
---
+++ FE/app/src/main/java/com/ssafy/heritage/view/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.heritage.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.heritage.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
