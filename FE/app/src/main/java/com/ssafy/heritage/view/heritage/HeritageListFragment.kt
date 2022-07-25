24e03a761e4f441022eb8dd0ac4f3f55c8b7c787
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-07-25 20:55:48 +0900

    [S07P12D102-24] FEAT : ListAdapter 예시 작성
    HeritageListAdapter 작성

--- FE/app/src/main/java/com/ssafy/heritage/view/heritage/HeritageListFragment.kt
+++ FE/app/src/main/java/com/ssafy/heritage/view/heritage/HeritageListFragment.kt
@@ -1,6 +1,7 @@
 package com.ssafy.heritage.view.heritage
 
 import com.ssafy.heritage.R
+import com.ssafy.heritage.adpter.HeritageListAdapter
 import com.ssafy.heritage.base.BaseFragment
 import com.ssafy.heritage.databinding.FragmentHeritageListBinding
 
@@ -8,7 +9,17 @@ private const val TAG = "HeritageListFragment___"
 
 class HeritageListFragment :
     BaseFragment<FragmentHeritageListBinding>(R.layout.fragment_heritage_list) {
+
+    private val heritageAdapter: HeritageListAdapter by lazy { HeritageListAdapter() }
+
     override fun init() {
-        TODO("Not yet implemented")
+
+        initAdapter()
+    }
+
+    private fun initAdapter() = with(binding) {
+        recyclerview.adapter = heritageAdapter
+
+        // heritageAdapter.submitList(리스트)  // submitList로 리스트 넣어줌
     }
 }
