48a82e11b587100c37cb613da747bacf411122d5
DESKTOP-KVCQHCD\multicampus <mxxxxxji@gmail.com>
2022-07-25 23:47:07 +0900

    [S07P12D102-120] UPDATE : GroupService

---
+++ FE/app/src/main/java/com/ssafy/heritage/ApplicationClass.kt
@@ -0,0 +1,13 @@
+package com.ssafy.heritage
+
+import android.app.Application
+
+class ApplicationClass : Application() {
+    companion object{
+
+        // 주소
+        const val BASE_URL=""
+        const val GROUP_URL=""
+    }
+
+}
