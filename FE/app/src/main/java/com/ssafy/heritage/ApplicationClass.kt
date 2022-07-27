1cd766a222eb8e8634421819232ec8ad080ac6f9
DESKTOP-C5AALBF\enddl <aud0725_123@naver.com>
2022-07-27 15:01:55 +0900

    [S07P12D102-170] UPDATE : ApplicationClass

--- FE/app/src/main/java/com/ssafy/heritage/ApplicationClass.kt
+++ FE/app/src/main/java/com/ssafy/heritage/ApplicationClass.kt
@@ -2,20 +2,22 @@ package com.ssafy.heritage
 
 import android.app.Application
 import com.ssafy.heritage.data.repository.Repository
+import com.ssafy.heritage.util.SharedPreferencesUtil
 
 class ApplicationClass : Application() {
     companion object{
-
+        lateinit var sharedPreferencesUtil: SharedPreferencesUtil
         // 주소
         const val BASE_URL=""
-        //const val GROUP_URL=""
     }
     override fun onCreate() {
         super.onCreate()
 
         initRepository()
-    }
 
+        // shared preference 초기화
+        sharedPreferencesUtil = SharedPreferencesUtil(this)
+    }
     private fun initRepository(){
         Repository.initialize(this)
     }
