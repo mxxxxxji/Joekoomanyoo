266667729423d5ca0619a24117adbacd20b22b34
Jaehwany <eejh22@gmail.com>
2022-07-27 18:56:37 +0900

    [] FIX : 모임 관리
    Entity 수정

--- BE/CommonProject/src/main/java/com/project/common/entity/GroupAttributeEntity.java
+++ BE/CommonProject/src/main/java/com/project/common/entity/GroupAttributeEntity.java
@@ -17,7 +17,6 @@ public class GroupAttributeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long gaSeq;
     
-
     @JoinColumn(name="group_seq",nullable=false)
 	private Long groupSeq;
 
---
+++ BE/CommonProject/src/main/java/com/project/common/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.project.common.repository;
+
+import com.project.common.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// entity와 연결해서 repository 생성해주면 jpa가 생성된 것이다.
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    UserEntity findByUserId(String username);
+
+    Optional<UserEntity> findOneByUserId(String userId);
+
+    void deleteByUserId(String userId);
+
+    UserEntity findByUserNickname(String userNickname);
+}
