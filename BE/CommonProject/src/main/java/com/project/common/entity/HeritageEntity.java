bdf943da2ec404c21059a6066d66467fe531fd0b
Jaehwany <eejh22@gmail.com>
2022-08-02 18:24:01 +0900

    [S07P12D102-119] UPDATE : 문화 유산 목록 배포

---
+++ BE/CommonProject/src/main/java/com/project/common/entity/HeritageEntity.java
@@ -0,0 +1,51 @@
+package com.project.common.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@ToString
+@Data
+// 파라미터가 없는 기본 생성자 생성
+@NoArgsConstructor
+// 모든 필드 값을 파라미터로 받는 생성자 생성
+@AllArgsConstructor
+@Table(name = "tb_heritage")
+public class HeritageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int heritageSeq;
+
+    @Column(length = 50, nullable = false)
+    private String heritageName;
+
+    @Column
+    private String heritageEra;
+    @Column(length = 50, nullable = false)
+    private String heritageAddress;
+    @Column
+    private String heritageCategory;
+    @Column(length = 30, nullable = false)
+    private String heritageLng;
+    @Column(length = 30, nullable = false)
+    private String heritageLat;
+    @Column
+    private String heritageImgUrl;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String heritageMemo;
+    @Column(columnDefinition = "TEXT")
+    private String heritageVoice;
+    @Column
+    private char stampExist;
+    @Column
+    private String heritageClass;
+    @Column
+    private int heritageScrapCnt;
+    @Column
+    private int heritageReviewCnt;
+
+
+}
