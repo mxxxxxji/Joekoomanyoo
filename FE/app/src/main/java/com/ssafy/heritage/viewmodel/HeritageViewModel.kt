1012548feaee752c45e575bab786a429b9c32ac6
foursoob <foursoob@naver.com>
2022-08-04 02:27:27 +0900

    [] FEAT: 문화유산 상세 리뷰 api 연결 중

--- FE/app/src/main/java/com/ssafy/heritage/viewmodel/HeritageViewModel.kt
+++ FE/app/src/main/java/com/ssafy/heritage/viewmodel/HeritageViewModel.kt
@@ -6,7 +6,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ssafy.heritage.data.dto.Heritage
+import com.ssafy.heritage.data.dto.HeritageReview
+import com.ssafy.heritage.data.remote.response.HeritageReviewListResponse
 import com.ssafy.heritage.data.repository.Repository
+import com.ssafy.heritage.util.SingleLiveEvent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -23,6 +26,15 @@ class HeritageViewModel : ViewModel() {
     private val _heritage = MutableLiveData<Heritage>()
     val heritage: LiveData<Heritage>
         get() = _heritage
+
+    private val _heritageReviewList = MutableLiveData<List<HeritageReviewListResponse>>()
+    val heritageReviewList: MutableLiveData<List<HeritageReviewListResponse>>
+        get() = _heritageReviewList
+
+    private val _insertHeritageReview = SingleLiveEvent<HeritageReviewListResponse>()
+    val insertHeritageReview: LiveData<HeritageReviewListResponse>
+        get() = _insertHeritageReview
+
 
     // 전체 문화유산 목록 가져옴
     fun getHeritageList() {
@@ -47,5 +59,34 @@ class HeritageViewModel : ViewModel() {
     // 더미 테스트용
     fun test(list: List<Heritage>) {
         _heritageList.value = list
+    }
+
+    // 문화유산 상세 리뷰 목록 가져오기
+    fun getHeritageReviewList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.selectAllHeritageReviews().let { response ->
+                if(response.isSuccessful){
+                    var list = response.body()!! as MutableList<HeritageReviewListResponse>
+                    list.sortBy { it.heritageReviewRegistedAt }  // 등록최신순
+                    _heritageReviewList.postValue(list)
+                }else{
+                    Log.d(TAG, "${response.code()}")
+                }
+            }
+        }
+    }
+
+    // 문화유산 상세 리뷰 작성
+    fun insertHeritageReview(heritageReviewInfo: HeritageReviewListResponse) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertHeritageReview(heritageReviewInfo).let { response ->
+                if (response.isSuccessful) {
+                    var info = response.body()!! as HeritageReviewListResponse
+                    _insertHeritageReview.postValue(info)
+                } else {
+                    Log.d(TAG, "${response.code()}")
+                }
+            }
+        }
     }
 }
