82c8eeb1d51552102dbb5d78eb96f6d5c001ba31
DESKTOP-KVCQHCD\multicampus <mxxxxxji@gmail.com>
2022-08-03 20:09:53 +0900

    [S07P12D102-120] UPDATE :  Member 조회 파일 추가

--- FE/app/src/main/java/com/ssafy/heritage/data/dto/Member.kt
+++ FE/app/src/main/java/com/ssafy/heritage/data/dto/Member.kt
@@ -1,4 +1,11 @@
 package com.ssafy.heritage.data.dto
 
-class Member {
-}
+import android.provider.ContactsContract
+
+data class Member(
+    val groupSeq: Int,
+    var memberAppeal: String,
+    var memberIsEvaluated: String,
+    var memberStatus: Int,
+    val userNickname: Int
+)
