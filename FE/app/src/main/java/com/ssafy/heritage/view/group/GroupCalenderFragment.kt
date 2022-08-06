30a37f15ab1ae3556a0ff7777bae3336dec75508
DESKTOP-C5AALBF\enddl <aud0725_123@naver.com>
2022-08-06 21:03:13 +0900

    [S07P12D102-185] UPDATE : CANLENDER 수정

--- FE/app/src/main/java/com/ssafy/heritage/view/group/GroupCalenderFragment.kt
+++ FE/app/src/main/java/com/ssafy/heritage/view/group/GroupCalenderFragment.kt
@@ -1,7 +1,11 @@
 package com.ssafy.heritage.view.group
 
+import android.os.Bundle
+import android.util.Log
+import com.events.calendar.utils.EventsCalendarUtil
 import com.events.calendar.utils.EventsCalendarUtil.MULTIPLE_SELECTION
 import com.events.calendar.utils.EventsCalendarUtil.SINGLE_SELECTION
+import com.events.calendar.utils.EventsCalendarUtil.getDateString
 import com.events.calendar.utils.EventsCalendarUtil.today
 import com.events.calendar.views.EventsCalendar
 import com.ssafy.heritage.R
@@ -31,17 +35,72 @@ class GroupCalenderFragment : BaseFragment<FragmentGroupCalendarBinding>(R.layou
             .build()
 
     }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val today = Calendar.getInstance()
 
+        val start = Calendar.getInstance()
+        start.add(Calendar.YEAR, -10)
+
+        val end = Calendar.getInstance()
+        end.add(Calendar.YEAR, 20)
+
+        binding.eventsCalendar.setSelectionMode(SINGLE_SELECTION)
+            .setToday(today)
+            .setMonthRange(today, end)
+            .setWeekStartDay(Calendar.SUNDAY, false)
+//            .setDatesTypeface(FontsManager.getTypeface(FontsManager.OPENSANS_REGULAR, requireContext()))
+//            .setMonthTitleTypeface(FontsManager.getTypeface(FontsManager.OPENSANS_SEMIBOLD, requireContext()))
+//            .setWeekHeaderTypeface(FontsManager.getTypeface(FontsManager.OPENSANS_SEMIBOLD, requireContext()))
+            .setCallback(this)
+            .build()
+
+        binding.eventsCalendar.post {
+            binding.eventsCalendar.setCurrentSelectedDate(today)
+        }
+
+        val c = Calendar.getInstance()
+        c.add(Calendar.DAY_OF_MONTH, 2)
+        binding.eventsCalendar.addEvent(c)
+        c.add(Calendar.DAY_OF_MONTH, 3)
+        binding.eventsCalendar.addEvent(c)
+        c.add(Calendar.DAY_OF_MONTH, 4)
+        binding.eventsCalendar.addEvent(c)
+        c.add(Calendar.DAY_OF_MONTH, 7)
+        binding.eventsCalendar.addEvent(c)
+    }
     override fun onDayLongPressed(selectedDate: Calendar?) {
 
     }
 
     override fun onDaySelected(selectedDate: Calendar?) {
         Log.e("CLICKED", EventsCalendarUtil.getDateString(selectedDate, EventsCalendarUtil.DD_MM_YYYY))
-        selected.text = getDateString(selectedDate?.timeInMillis)
+        binding.selected.text = getDateString(selectedDate?.timeInMillis)
     }
 
     override fun onMonthChanged(monthStartDate: Calendar?) {
 
+    }
+    private fun getDateString(time: Long?): String {
+        if (time != null) {
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = time
+            val month = when (cal[Calendar.MONTH]) {
+                Calendar.JANUARY -> "January"
+                Calendar.FEBRUARY -> "February"
+                Calendar.MARCH -> "March"
+                Calendar.APRIL -> "April"
+                Calendar.MAY -> "May"
+                Calendar.JUNE -> "June"
+                Calendar.JULY -> "July"
+                Calendar.AUGUST -> "August"
+                Calendar.SEPTEMBER -> "September"
+                Calendar.OCTOBER -> "October"
+                Calendar.NOVEMBER -> "November"
+                Calendar.DECEMBER -> "December"
+                else -> ""
+            }
+            return "$month ${cal[Calendar.DAY_OF_MONTH]}, ${cal[Calendar.YEAR]}"
+        } else return ""
     }
 }
