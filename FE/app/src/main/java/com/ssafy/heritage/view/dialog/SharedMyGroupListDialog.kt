5a8d71b5d3a53dbcfbdb56e16729f873573835c0
foursoob <foursoob@naver.com>
2022-08-07 15:47:44 +0900

    [S07P12D102-161] FEAT: 공유하기 다이얼로그 화면 구성

---
+++ FE/app/src/main/java/com/ssafy/heritage/view/dialog/SharedMyGroupListDialog.kt
@@ -0,0 +1,34 @@
+package com.ssafy.heritage.view.dialog
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.ssafy.heritage.databinding.DialogSharedMyGroupListBinding
+import com.ssafy.heritage.viewmodel.UserViewModel
+
+private const val TAG = "SharedMyGroupListDialog___"
+
+class SharedMyGroupListDialog : DialogFragment() {
+    var binding: DialogSharedMyGroupListBinding? = null
+    private val userViewModel by activityViewModels<UserViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+//        binding = DialogKeywordBinding.inflate(inflater, container, false)
+
+        // 다이얼로그 뒤에 배경 흐리게
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+        return binding!!.root
+    }
+}
