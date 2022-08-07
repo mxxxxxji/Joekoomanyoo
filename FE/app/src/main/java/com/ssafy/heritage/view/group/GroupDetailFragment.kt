d164e428c558b9d1a3c03a14abadfba7349cf726
DESKTOP-KVCQHCD\multicampus <mxxxxxji@gmail.com>
2022-08-07 21:49:41 +0900

    [S07P12D102-257] UPDATE : Adapter 수정 ListAdapter로 수정

--- FE/app/src/main/java/com/ssafy/heritage/view/group/GroupDetailFragment.kt
+++ FE/app/src/main/java/com/ssafy/heritage/view/group/GroupDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.ssafy.heritage.view.group
 
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -14,8 +15,11 @@ import com.ssafy.heritage.data.remote.request.GroupBasic
 import com.ssafy.heritage.data.remote.request.GroupJoin
 import com.ssafy.heritage.data.remote.response.GroupListResponse
 import com.ssafy.heritage.databinding.FragmentGroupDetailBinding
+import com.ssafy.heritage.listener.MemberClickListener
 import com.ssafy.heritage.view.dialog.ApplyGroupJoinDialog
 import com.ssafy.heritage.view.dialog.ApplyGroupJoinDialogInterface
+import com.ssafy.heritage.view.dialog.OtherProfileDialog
+import com.ssafy.heritage.view.dialog.OtherProfileDialogInterface
 import com.ssafy.heritage.viewmodel.GroupViewModel
 import com.ssafy.heritage.viewmodel.UserViewModel
 
@@ -23,12 +27,12 @@ private const val TAG = " GroupDetailFragment___"
 
 class GroupDetailFragment :
     BaseFragment<FragmentGroupDetailBinding>(R.layout.fragment_group_detail),
-    OnItemClickListener, ApplyGroupJoinDialogInterface {
+    ApplyGroupJoinDialogInterface, OtherProfileDialogInterface {
 
     private val groupViewModel by activityViewModels<GroupViewModel>()
     private val userSeq: Int = ApplicationClass.sharedPreferencesUtil.getUser()
-    private lateinit var memberAdapter: MemberAdapter
-    private lateinit var applicantAdapter: MemberAdapter
+    private val memberAdapter: MemberAdapter by lazy {MemberAdapter()}
+    private val applicantAdapter: MemberAdapter by lazy {MemberAdapter()}
     private lateinit var groupInfo: GroupListResponse
 
     override fun init() {
@@ -39,11 +43,26 @@ class GroupDetailFragment :
         initClickListener()
     }
 
-    private fun initAdapter() {
-        memberAdapter = MemberAdapter(this)
-        applicantAdapter = MemberAdapter(this)
-        binding.recyclerviewMembers.adapter = memberAdapter
-        binding.recyclerviewApplicant.adapter = applicantAdapter
+    private fun initAdapter(){
+        binding.recyclerviewMembers.apply {
+            adapter = memberAdapter
+            memberAdapter.memberClickListener = object : MemberClickListener {
+                override fun onClick(position: Int, member: Member, view: View) {
+                    val dialog = OtherProfileDialog(requireContext(), member, userPermission= groupViewModel.groupPermission.value!!,this@GroupDetailFragment)
+                    dialog.show()
+                }
+            }
+        }
+        binding.recyclerviewApplicant.apply {
+            adapter = applicantAdapter
+            memberAdapter.memberClickListener = object : MemberClickListener {
+                override fun onClick(position: Int, member: Member, view: View) {
+                    val dialog = OtherProfileDialog(requireContext(), member,  userPermission= groupViewModel.groupPermission.value!!, this@GroupDetailFragment)
+                    dialog.show()
+                }
+
+            }
+        }
     }
 
     private fun initObserver() = with(binding) {
@@ -102,9 +121,7 @@ class GroupDetailFragment :
         // 가입취소
         binding.btnCancellation.setOnClickListener {
             // 모임을 떠나시겠습니까? 다이얼로그
-            groupViewModel.leaveGroupJoin(
-                groupInfo.groupSeq
-            )
+            groupViewModel.leaveGroupJoin(groupInfo.groupSeq, userSeq)
             Toast.makeText(requireActivity(), "가입이 취소되었습니다.", Toast.LENGTH_SHORT).show()
             groupViewModel.setGroupPermission(3)
             parentFragment?.findNavController()?.popBackStack()
@@ -114,21 +131,19 @@ class GroupDetailFragment :
         // 탈퇴하기
         binding.btnDrop.setOnClickListener {
             // 모임을 떠나시겠습니까? 다이얼로그
-            groupViewModel.leaveGroupJoin(
-                groupInfo.groupSeq
-            )
+            groupViewModel.leaveGroupJoin(groupInfo.groupSeq, userSeq)
             Toast.makeText(requireActivity(), "모임을 탈퇴했습니다.", Toast.LENGTH_SHORT).show()
             groupViewModel.setGroupPermission(3)
             val action =
                 GroupDetailFragmentDirections.actionGroupDetailFragmentToGroupListFragment()
             findNavController().navigate(action)
         }
-        // 모임시작
-        // 모임종료
-        // 모임삭제
-    }
 
-    override fun onItemClick(position: Int) {
+        // 모임시작
+
+        // 모임종료
+
+        // 모임삭제
     }
 
     override fun onOkBtnClicked(appeal: String) {
@@ -136,5 +151,26 @@ class GroupDetailFragment :
         Log.d(TAG, "가입을 요청했습니다.")
         Toast.makeText(requireActivity(), "가입을 요청했습니다.", Toast.LENGTH_SHORT).show()
         groupViewModel.setGroupPermission(0)
+        // 방장에게 알림 전송
+    }
+
+    override fun onApproveBtnClicked(userSeq: Int) {
+        groupViewModel.approveGroupJoin(groupInfo.groupSeq, GroupBasic(userSeq,groupInfo.groupSeq))
+        Log.d(TAG, "가입을 승인했습니다")
+        Toast.makeText(requireActivity(), "가입을 승인했습니다", Toast.LENGTH_SHORT).show()
+        // 그 회원에게 알림 전송
+    }
+
+    override fun onRefuseBtnClicked(userSeq: Int) {
+        groupViewModel.leaveGroupJoin(groupInfo.groupSeq, userSeq)
+        Log.d(TAG, "가입을 요청을 거절했습니다.")
+        Toast.makeText(requireActivity(), "가입을 요청을 거절했습니다.", Toast.LENGTH_SHORT).show()
+        // 그 회원에게 알림 전송
+    }
+
+    override fun onDropBtnClicked(userSeq: Int) {
+        groupViewModel.leaveGroupJoin(groupInfo.groupSeq, userSeq)
+        Toast.makeText(requireActivity(), "${groupInfo.groupName}님이 퇴장되었습니다", Toast.LENGTH_SHORT).show()
+        // 그 회원에게 알림 전송
     }
 }
