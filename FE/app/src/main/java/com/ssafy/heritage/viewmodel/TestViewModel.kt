7b0e804573926bf09528620caed632d5f6a7519a
DESKTOP-KVCQHCD\multicampus <mxxxxxji@gmail.com>
2022-08-09 17:22:05 +0900

    [S07P12D102-180] FEAT : 이미지 전송 TEST

---
+++ FE/app/src/main/java/com/ssafy/heritage/viewmodel/TestViewModel.kt
@@ -0,0 +1,43 @@
+package com.ssafy.heritage.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.ssafy.heritage.data.repository.Repository
+import com.ssafy.heritage.util.FormDataUtil
+import com.ssafy.heritage.util.SingleLiveEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import org.w3c.dom.Comment
+import java.io.File
+
+private const val TAG = "TestViewModel___"
+class TestViewModel : ViewModel() {
+
+    private val repository = Repository.get()
+    private val _reviewState = SingleLiveEvent<Boolean>()
+    val reviewState: LiveData<Boolean>
+        get() = _reviewState
+
+    fun saveImage(file: File) {
+        val img = FormDataUtil.getImageBody("file", file)
+        viewModelScope.launch(Dispatchers.Main)  {
+            repository.saveImage(
+                img
+            ).let { response ->
+                if(response.isSuccessful){
+                    _reviewState.postValue(response.body())
+                    Log.d(TAG, "성공uploadChat: ${response.code()}")
+                }else{
+                    _reviewState.postValue(response.body())
+                    Log.e("uploadChat()", "에러 : " + response.body())
+                    Log.d(TAG, "실패uploadChat: ${response.code()}")
+                }
+            }
+        }
+    }
+}
