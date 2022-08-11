39316b1f0816d8000a8fbef40f8977825d0b618d
DESKTOP-KVCQHCD\multicampus <mxxxxxji@gmail.com>
2022-08-11 11:21:14 +0900

    Revert "[S07P12D102-300] UPDATE : 오브젝트 및 AR 기본 기능"
    This reverts commit 6e7789444febbb83bc76fa9e99e126f0ecaeadd5.

--- FE/app/src/main/java/com/ssafy/heritage/HelloGeoActivity.kt
+++ FE/app/src/main/java/com/ssafy/heritage/HelloGeoActivity.kt
@@ -1,3 +1,4 @@
+
 package com.ssafy.heritage
 
 import android.os.Bundle
@@ -13,88 +14,84 @@ import com.ssafy.heritage.helpers.GeoPermissionsHelper
 import com.ssafy.heritage.samplerender.SampleRender
 
 class HelloGeoActivity : AppCompatActivity() {
-    companion object {
-        private const val TAG = "HelloGeoActivity"
+  companion object {
+    private const val TAG = "HelloGeoActivity"
+  }
+
+  lateinit var arCoreSessionHelper: ARCoreSessionLifecycleHelper
+  lateinit var view: HelloGeoView
+  lateinit var renderer: HelloGeoRenderer
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    // Setup ARCore session lifecycle helper and configuration.
+    arCoreSessionHelper = ARCoreSessionLifecycleHelper(this)
+    // If Session creation or Session.resume() fails, display a message and log detailed
+    // information.
+    arCoreSessionHelper.exceptionCallback =
+      { exception ->
+        val message =
+          when (exception) {
+            is UnavailableUserDeclinedInstallationException ->
+              "Please install Google Play Services for AR"
+            is UnavailableApkTooOldException -> "Please update ARCore"
+            is UnavailableSdkTooOldException -> "Please update this app"
+            is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
+            is CameraNotAvailableException -> "Camera not available. Try restarting the app."
+            else -> "Failed to create AR session: $exception"
+          }
+        Log.e(TAG, "ARCore threw an exception", exception)
+        view.snackbarHelper.showError(this, message)
+      }
+
+    // Configure session features.
+    arCoreSessionHelper.beforeSessionResume = ::configureSession
+    lifecycle.addObserver(arCoreSessionHelper)
+
+    // Set up the Hello AR renderer.
+    renderer = HelloGeoRenderer(this)
+    lifecycle.addObserver(renderer)
+
+    // Set up Hello AR UI.
+    view = HelloGeoView(this)
+    lifecycle.addObserver(view)
+    setContentView(view.root)
+
+    // Sets up an example renderer using our HelloGeoRenderer.
+    SampleRender(view.surfaceView, renderer, assets)
+  }
+
+  // Configure the session, setting the desired options according to your usecase.
+  fun configureSession(session: Session) {
+    session.configure(
+      session.config.apply {
+        // Enable Geospatial Mode.
+        geospatialMode = Config.GeospatialMode.ENABLED
+      }
+    )
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<String>,
+    results: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, results)
+    if (!GeoPermissionsHelper.hasGeoPermissions(this)) {
+      // Use toast instead of snackbar here since the activity will exit.
+      Toast.makeText(this, "Camera and location permissions are needed to run this application", Toast.LENGTH_LONG)
+        .show()
+      if (!GeoPermissionsHelper.shouldShowRequestPermissionRationale(this)) {
+        // Permission denied with checking "Do not ask again".
+        GeoPermissionsHelper.launchPermissionSettings(this)
+      }
+      finish()
     }
+  }
 
-    lateinit var arCoreSessionHelper: ARCoreSessionLifecycleHelper
-    lateinit var view: HelloGeoView
-    lateinit var renderer: HelloGeoRenderer
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Setup ARCore session lifecycle helper and configuration.
-        arCoreSessionHelper = ARCoreSessionLifecycleHelper(this)
-        // If Session creation or Session.resume() fails, display a message and log detailed
-        // information.
-        arCoreSessionHelper.exceptionCallback =
-            { exception ->
-                val message =
-                    when (exception) {
-                        is UnavailableUserDeclinedInstallationException ->
-                            "Please install Google Play Services for AR"
-                        is UnavailableApkTooOldException -> "Please update ARCore"
-                        is UnavailableSdkTooOldException -> "Please update this app"
-                        is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
-                        is CameraNotAvailableException -> "Camera not available. Try restarting the app."
-                        else -> "Failed to create AR session: $exception"
-                    }
-                Log.e(TAG, "ARCore threw an exception", exception)
-                view.snackbarHelper.showError(this, message)
-            }
-
-        // Configure session features.
-        arCoreSessionHelper.beforeSessionResume = ::configureSession
-        lifecycle.addObserver(arCoreSessionHelper)
-
-        // Set up the Hello AR renderer.
-        renderer = HelloGeoRenderer(this)
-        lifecycle.addObserver(renderer)
-
-        // Set up Hello AR UI.
-        view = HelloGeoView(this)
-        lifecycle.addObserver(view)
-        setContentView(view.root)
-
-        // Sets up an example renderer using our HelloGeoRenderer.
-        SampleRender(view.surfaceView, renderer, assets)
-    }
-
-    // Configure the session, setting the desired options according to your usecase.
-    fun configureSession(session: Session) {
-        session.configure(
-            session.config.apply {
-                // Enable Geospatial Mode.
-                geospatialMode = Config.GeospatialMode.ENABLED
-            }
-        )
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        results: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, results)
-        if (!GeoPermissionsHelper.hasGeoPermissions(this)) {
-            // Use toast instead of snackbar here since the activity will exit.
-            Toast.makeText(
-                this,
-                "Camera and location permissions are needed to run this application",
-                Toast.LENGTH_LONG
-            )
-                .show()
-            if (!GeoPermissionsHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                GeoPermissionsHelper.launchPermissionSettings(this)
-            }
-            finish()
-        }
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
-    }
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
+  }
 }
--- FE/app/src/main/java/com/ssafy/heritage/HelloGeoRenderer.kt
+++ FE/app/src/main/java/com/ssafy/heritage/HelloGeoRenderer.kt
@@ -31,210 +31,207 @@ import java.io.IOException
 
 
 private const val TAG = "HelloGeoRenderer__"
+class HelloGeoRenderer(val activity: HelloGeoActivity) : SampleRender.Renderer, DefaultLifecycleObserver {
+  companion object {
+    private val Z_NEAR = 0.1f
+    private val Z_FAR = 1000f
+  }
 
-class HelloGeoRenderer(val activity: HelloGeoActivity) : SampleRender.Renderer,
-    DefaultLifecycleObserver {
-    companion object {
-        private val Z_NEAR = 0.1f
-        private val Z_FAR = 1000f
+  lateinit var backgroundRenderer: BackgroundRenderer
+  lateinit var virtualSceneFramebuffer: Framebuffer
+  var hasSetTextureNames = false
+
+  // Virtual object (ARCore pawn)
+  lateinit var virtualObjectMesh: Mesh
+  lateinit var virtualObjectShader: Shader
+  lateinit var virtualObjectTexture: Texture
+
+  // Temporary matrix allocated here to reduce number of allocations for each frame.
+  val modelMatrix = FloatArray(16)
+  val viewMatrix = FloatArray(16)
+  val projectionMatrix = FloatArray(16)
+  val modelViewMatrix = FloatArray(16) // view x model
+
+  val modelViewProjectionMatrix = FloatArray(16) // projection x view x model
+
+  val session
+    get() = activity.arCoreSessionHelper.session
+
+  val displayRotationHelper = DisplayRotationHelper(activity)
+  val trackingStateHelper = TrackingStateHelper(activity)
+
+  override fun onResume(owner: LifecycleOwner) {
+    displayRotationHelper.onResume()
+    hasSetTextureNames = false
+  }
+
+  override fun onPause(owner: LifecycleOwner) {
+    displayRotationHelper.onPause()
+  }
+
+  override fun onSurfaceCreated(render: SampleRender) {
+    // Prepare the rendering objects.
+    // This involves reading shaders and 3D model files, so may throw an IOException.
+    try {
+      backgroundRenderer = BackgroundRenderer(render)
+      virtualSceneFramebuffer = Framebuffer(render, /*width=*/ 1, /*height=*/ 1)
+
+      // Virtual object to render (Geospatial Marker)
+      virtualObjectTexture =
+        Texture.createFromAsset(
+          render,
+          "models/spatial_marker_baked.png",
+          Texture.WrapMode.CLAMP_TO_EDGE,
+          Texture.ColorFormat.SRGB
+        )
+
+      virtualObjectMesh = Mesh.createFromAsset(render, "models/geospatial_marker.obj");
+      virtualObjectShader =
+        Shader.createFromAssets(
+          render,
+          "shaders/ar_unlit_object.vert",
+          "shaders/ar_unlit_object.frag",
+          /*defines=*/ null)
+          .setTexture("u_Texture", virtualObjectTexture)
+
+      backgroundRenderer.setUseDepthVisualization(render, false)
+      backgroundRenderer.setUseOcclusion(render, false)
+    } catch (e: IOException) {
+      Log.e(TAG, "Failed to read a required asset file", e)
+      showError("Failed to read a required asset file: $e")
+    }
+  }
+
+  override fun onSurfaceChanged(render: SampleRender, width: Int, height: Int) {
+    displayRotationHelper.onSurfaceChanged(width, height)
+    virtualSceneFramebuffer.resize(width, height)
+  }
+  //</editor-fold>
+
+  override fun onDrawFrame(render: SampleRender) {
+    val session = session ?: return
+
+    //<editor-fold desc="ARCore frame boilerplate" defaultstate="collapsed">
+    // Texture names should only be set once on a GL thread unless they change. This is done during
+    // onDrawFrame rather than onSurfaceCreated since the session is not guaranteed to have been
+    // initialized during the execution of onSurfaceCreated.
+    if (!hasSetTextureNames) {
+      session.setCameraTextureNames(intArrayOf(backgroundRenderer.cameraColorTexture.textureId))
+      hasSetTextureNames = true
     }
 
-    lateinit var backgroundRenderer: BackgroundRenderer
-    lateinit var virtualSceneFramebuffer: Framebuffer
-    var hasSetTextureNames = false
+    // -- Update per-frame state
 
-    // Virtual object (ARCore pawn)
-    lateinit var virtualObjectMesh: Mesh
-    lateinit var virtualObjectShader: Shader
-    lateinit var virtualObjectTexture: Texture
+    // Notify ARCore session that the view size changed so that the perspective matrix and
+    // the video background can be properly adjusted.
+    displayRotationHelper.updateSessionIfNeeded(session)
 
-    // Temporary matrix allocated here to reduce number of allocations for each frame.
-    val modelMatrix = FloatArray(16)
-    val viewMatrix = FloatArray(16)
-    val projectionMatrix = FloatArray(16)
-    val modelViewMatrix = FloatArray(16) // view x model
+    // Obtain the current frame from ARSession. When the configuration is set to
+    // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+    // camera framerate.
+    val frame =
+      try {
+        session.update()
+      } catch (e: CameraNotAvailableException) {
+        Log.e(TAG, "Camera not available during onDrawFrame", e)
+        showError("Camera not available. Try restarting the app.")
+        return
+      }
 
-    val modelViewProjectionMatrix = FloatArray(16) // projection x view x model
+    val camera = frame.camera
 
-    val session
-        get() = activity.arCoreSessionHelper.session
+    // BackgroundRenderer.updateDisplayGeometry must be called every frame to update the coordinates
+    // used to draw the background camera image.
+    backgroundRenderer.updateDisplayGeometry(frame)
 
-    val displayRotationHelper = DisplayRotationHelper(activity)
-    val trackingStateHelper = TrackingStateHelper(activity)
+    // Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
+    trackingStateHelper.updateKeepScreenOnFlag(camera.trackingState)
 
-    override fun onResume(owner: LifecycleOwner) {
-        displayRotationHelper.onResume()
-        hasSetTextureNames = false
+    // -- Draw background
+    if (frame.timestamp != 0L) {
+      // Suppress rendering if the camera did not produce the first frame yet. This is to avoid
+      // drawing possible leftover data from previous sessions if the texture is reused.
+      backgroundRenderer.drawBackground(render)
     }
 
-    override fun onPause(owner: LifecycleOwner) {
-        displayRotationHelper.onPause()
+    // If not tracking, don't draw 3D objects.
+    if (camera.trackingState == TrackingState.PAUSED) {
+      return
     }
 
-    override fun onSurfaceCreated(render: SampleRender) {
-        // Prepare the rendering objects.
-        // This involves reading shaders and 3D model files, so may throw an IOException.
-        try {
-            backgroundRenderer = BackgroundRenderer(render)
-            virtualSceneFramebuffer = Framebuffer(render, /*width=*/ 1, /*height=*/ 1)
+    // Get projection matrix.
+    camera.getProjectionMatrix(projectionMatrix, 0, Z_NEAR, Z_FAR)
 
-            // Virtual object to render (Geospatial Marker)
-            virtualObjectTexture =
-                Texture.createFromAsset(
-                    render,
-                    "models/spatial_marker_baked.png",
-                    Texture.WrapMode.CLAMP_TO_EDGE,
-                    Texture.ColorFormat.SRGB
-                )
+    // Get camera matrix and draw.
+    camera.getViewMatrix(viewMatrix, 0)
 
-            virtualObjectMesh = Mesh.createFromAsset(render, "models/geospatial_marker.obj");
-            virtualObjectShader =
-                Shader.createFromAssets(
-                    render,
-                    "shaders/ar_unlit_object.vert",
-                    "shaders/ar_unlit_object.frag",
-                    /*defines=*/ null
-                )
-                    .setTexture("u_Texture", virtualObjectTexture)
-
-            backgroundRenderer.setUseDepthVisualization(render, false)
-            backgroundRenderer.setUseOcclusion(render, false)
-        } catch (e: IOException) {
-            Log.e(TAG, "Failed to read a required asset file", e)
-            showError("Failed to read a required asset file: $e")
-        }
-    }
-
-    override fun onSurfaceChanged(render: SampleRender, width: Int, height: Int) {
-        displayRotationHelper.onSurfaceChanged(width, height)
-        virtualSceneFramebuffer.resize(width, height)
-    }
+    render.clear(virtualSceneFramebuffer, 0f, 0f, 0f, 0f)
     //</editor-fold>
 
-    override fun onDrawFrame(render: SampleRender) {
-        val session = session ?: return
-
-        //<editor-fold desc="ARCore frame boilerplate" defaultstate="collapsed">
-        // Texture names should only be set once on a GL thread unless they change. This is done during
-        // onDrawFrame rather than onSurfaceCreated since the session is not guaranteed to have been
-        // initialized during the execution of onSurfaceCreated.
-        if (!hasSetTextureNames) {
-            session.setCameraTextureNames(intArrayOf(backgroundRenderer.cameraColorTexture.textureId))
-            hasSetTextureNames = true
-        }
-
-        // -- Update per-frame state
-
-        // Notify ARCore session that the view size changed so that the perspective matrix and
-        // the video background can be properly adjusted.
-        displayRotationHelper.updateSessionIfNeeded(session)
-
-        // Obtain the current frame from ARSession. When the configuration is set to
-        // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
-        // camera framerate.
-        val frame =
-            try {
-                session.update()
-            } catch (e: CameraNotAvailableException) {
-                Log.e(TAG, "Camera not available during onDrawFrame", e)
-                showError("Camera not available. Try restarting the app.")
-                return
-            }
-
-        val camera = frame.camera
-
-        // BackgroundRenderer.updateDisplayGeometry must be called every frame to update the coordinates
-        // used to draw the background camera image.
-        backgroundRenderer.updateDisplayGeometry(frame)
-
-        // Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
-        trackingStateHelper.updateKeepScreenOnFlag(camera.trackingState)
-
-        // -- Draw background
-        if (frame.timestamp != 0L) {
-            // Suppress rendering if the camera did not produce the first frame yet. This is to avoid
-            // drawing possible leftover data from previous sessions if the texture is reused.
-            backgroundRenderer.drawBackground(render)
-        }
-
-        // If not tracking, don't draw 3D objects.
-        if (camera.trackingState == TrackingState.PAUSED) {
-            return
-        }
-
-        // Get projection matrix.
-        camera.getProjectionMatrix(projectionMatrix, 0, Z_NEAR, Z_FAR)
-
-        // Get camera matrix and draw.
-        camera.getViewMatrix(viewMatrix, 0)
-
-        render.clear(virtualSceneFramebuffer, 0f, 0f, 0f, 0f)
-        //</editor-fold>
-
-        // 어스 객체를 사용할 수 있는지 확인
-        val earth = session.earth
-        if (earth?.trackingState == TrackingState.TRACKING) {
-            // 위도와 경도로 표현된 위치, 정확성에 대한 추정치도 제공
-            // 고도와 고고의 추정치
-            // 방향, 기기의 현재 방향에 대한 근사치와 방향의 추정치
-            val cameraGeospatialPose = earth.cameraGeospatialPose
-            activity.view.mapView?.updateMapPosition(
-                latitude = cameraGeospatialPose.latitude,
-                longitude = cameraGeospatialPose.longitude,
-                heading = cameraGeospatialPose.heading
-            )
-        }
-
-        // Draw the placed anchor, if it exists.
-        earthAnchor?.let {
-            render.renderCompassAtAnchor(it)
-        }
-
-        // Compose the virtual scene with the background.
-        backgroundRenderer.drawVirtualScene(render, virtualSceneFramebuffer, Z_NEAR, Z_FAR)
+    // 어스 객체를 사용할 수 있는지 확인
+    val earth = session.earth
+    if (earth?.trackingState == TrackingState.TRACKING) {
+      // 위도와 경도로 표현된 위치, 정확성에 대한 추정치도 제공
+      // 고도와 고고의 추정치
+      // 방향, 기기의 현재 방향에 대한 근사치와 방향의 추정치
+      val cameraGeospatialPose = earth.cameraGeospatialPose
+      activity.view.mapView?.updateMapPosition(
+        latitude = cameraGeospatialPose.latitude,
+        longitude = cameraGeospatialPose.longitude,
+        heading = cameraGeospatialPose.heading
+      )
     }
 
-    var earthAnchor: Anchor? = null
-
-    fun onMapClick(latLng: LatLng) {
-        val earth = session?.earth ?: return
-        // 객체가 추적 중인지 확인
-        if (earth.trackingState != TrackingState.TRACKING) {
-            return
-        }
-        // earthAnchor가 있으면 분리
-        earthAnchor?.detach()
-
-        // (임시) 어스 앵커를 카메라와 같은 높이데 둔다
-        val altitude = earth.cameraGeospatialPose.altitude - 1
-        // 좌표계에서 앵커의 회전 방향
-        val qx = 0f
-        val qy = 0f
-        val qz = 0f
-        val qw = 1f
-        earthAnchor =
-            earth.createAnchor(latLng.latitude, latLng.longitude, altitude, qx, qy, qz, qw)
-
-        activity.view.mapView?.earthMarker?.apply {
-            position = latLng
-            isVisible = true
-        }
+    // Draw the placed anchor, if it exists.
+    earthAnchor?.let {
+      render.renderCompassAtAnchor(it)
     }
 
-    private fun SampleRender.renderCompassAtAnchor(anchor: Anchor) {
-        // Get the current pose of the Anchor in world space. The Anchor pose is updated
-        // during calls to session.update() as ARCore refines its estimate of the world.
-        anchor.pose.toMatrix(modelMatrix, 0)
+    // Compose the virtual scene with the background.
+    backgroundRenderer.drawVirtualScene(render, virtualSceneFramebuffer, Z_NEAR, Z_FAR)
+  }
 
-        // Calculate model/view/projection matrices
-        Matrix.multiplyMM(modelViewMatrix, 0, viewMatrix, 0, modelMatrix, 0)
-        Matrix.multiplyMM(modelViewProjectionMatrix, 0, projectionMatrix, 0, modelViewMatrix, 0)
+  var earthAnchor: Anchor? = null
 
-        // Update shader properties and draw
-        virtualObjectShader.setMat4("u_ModelViewProjection", modelViewProjectionMatrix)
-        draw(virtualObjectMesh, virtualObjectShader, virtualSceneFramebuffer)
+  fun onMapClick(latLng: LatLng) {
+    val earth = session?.earth ?: return
+    // 객체가 추적 중인지 확인
+    if (earth.trackingState != TrackingState.TRACKING) {
+      return
     }
+    // earthAnchor가 있으면 분리
+    earthAnchor?.detach()
 
-    private fun showError(errorMessage: String) =
-        activity.view.snackbarHelper.showError(activity, errorMessage)
+    // (임시) 어스 앵커를 카메라와 같은 높이데 둔다
+    val altitude = earth.cameraGeospatialPose.altitude - 1
+    // 좌표계에서 앵커의 회전 방향
+    val qx = 0f
+    val qy = 0f
+    val qz = 0f
+    val qw = 1f
+    earthAnchor =
+      earth.createAnchor(latLng.latitude, latLng.longitude, altitude, qx, qy, qz, qw)
+
+    activity.view.mapView?.earthMarker?.apply {
+      position = latLng
+      isVisible = true
+    }
+  }
+
+  private fun SampleRender.renderCompassAtAnchor(anchor: Anchor) {
+    // Get the current pose of the Anchor in world space. The Anchor pose is updated
+    // during calls to session.update() as ARCore refines its estimate of the world.
+    anchor.pose.toMatrix(modelMatrix, 0)
+
+    // Calculate model/view/projection matrices
+    Matrix.multiplyMM(modelViewMatrix, 0, viewMatrix, 0, modelMatrix, 0)
+    Matrix.multiplyMM(modelViewProjectionMatrix, 0, projectionMatrix, 0, modelViewMatrix, 0)
+
+    // Update shader properties and draw
+    virtualObjectShader.setMat4("u_ModelViewProjection", modelViewProjectionMatrix)
+    draw(virtualObjectMesh, virtualObjectShader, virtualSceneFramebuffer)
+  }
+
+  private fun showError(errorMessage: String) =
+    activity.view.snackbarHelper.showError(activity, errorMessage)
 }
