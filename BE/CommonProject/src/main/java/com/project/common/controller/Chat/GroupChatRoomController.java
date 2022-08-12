1d1cc90a0d2fa3a9d0d61201ae002b1ea3f435b3
Jaehwany <eejh22@gmail.com>
2022-08-12 14:40:01 +0900

    S07P12D102-273] FIX : Chat Service divide

--- BE/CommonProject/src/main/java/com/project/common/controller/Chat/GroupChatRoomController.java
+++ BE/CommonProject/src/main/java/com/project/common/controller/Chat/GroupChatRoomController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.common.dto.Chat.GroupChatRoomDto;
-import com.project.common.service.Group.GroupChatService;
+import com.project.common.service.Group.GroupChatRoomService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,18 +20,18 @@ import lombok.RequiredArgsConstructor;
 @Api(tags = {"채팅 방 API"})
 public class GroupChatRoomController {
 
-    private final GroupChatService groupChatService;
+    private final GroupChatRoomService groupChatRoomService;
     
     @ApiOperation(value = "채팅방 개설")
     @PostMapping("/room")
     public ResponseEntity<GroupChatRoomDto> createChatRoom( @RequestParam int groupSeq){
-    	return new ResponseEntity<>(groupChatService.createChatRoom(groupSeq), HttpStatus.CREATED);
+    	return new ResponseEntity<>(groupChatRoomService.createChatRoom(groupSeq), HttpStatus.CREATED);
     }
    
     @ApiOperation(value = "채팅방 조회")
     @GetMapping("/room")
     public ResponseEntity<GroupChatRoomDto> ChatRoomInfo( @RequestParam int groupSeq){
-    	return new ResponseEntity<>(groupChatService.getChatRoomByGroupSeq(groupSeq), HttpStatus.OK);
+    	return new ResponseEntity<>(groupChatRoomService.getChatRoomByGroupSeq(groupSeq), HttpStatus.OK);
     }
 
 }
--- BE/CommonProject/src/main/java/com/project/common/service/Group/GroupChatService.java
+++ BE/CommonProject/src/main/java/com/project/common/service/Group/GroupChatService.java
@@ -1,16 +1,11 @@
 package com.project.common.service.Group;
 
 import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.project.common.dto.Chat.GroupChatDto;
-import com.project.common.dto.Chat.GroupChatRoomDto;
 import com.project.common.entity.Group.GroupChatEntity;
 import com.project.common.entity.Group.GroupEntity;
 import com.project.common.repository.Group.GroupRepository;
@@ -22,14 +17,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GroupChatService{
 
-    private Map<Integer, GroupChatRoomDto> chatRoomDTOMap;
     private GroupRepository groupRepository;
-    
-    @PostConstruct
-    private void init(){
-        chatRoomDTOMap = new LinkedHashMap<>();
-    }
-
+   
     public String saveMessage(GroupChatDto chat){
     	GroupEntity group = groupRepository.findByGroupSeq(chat.getGroupSeq());
     	
@@ -40,23 +29,8 @@ public class GroupChatService{
 				.userSeq(chat.getUserSeq())
 				.build());
 		groupRepository.save(group);
-    	
 
     	return "Success";
-    }
-    
-    
-    // 특정 채팅방 조회
-    public GroupChatRoomDto getChatRoomByGroupSeq(int groupSeq){
-        return chatRoomDTOMap.get(groupSeq);
-    }
-    
-    // 채팅방 생성
-    public GroupChatRoomDto createChatRoom(int groupSeq){
-    	GroupChatRoomDto room = GroupChatRoomDto.create(groupSeq);
-        chatRoomDTOMap.put(room.getGroupSeq(), room);
-
-        return room;
     }
     
 
