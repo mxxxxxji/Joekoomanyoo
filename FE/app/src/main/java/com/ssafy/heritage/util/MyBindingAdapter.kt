b7b29d67b74ae344ef07b14e687967518d81a36d
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-08-14 14:48:45 +0900

    [S07P12D102-295] UPDATE : 카테고리 오류 수정

--- FE/app/src/main/java/com/ssafy/heritage/util/MyBindingAdapter.kt
+++ FE/app/src/main/java/com/ssafy/heritage/util/MyBindingAdapter.kt
@@ -1,16 +1,23 @@
 package com.ssafy.heritage.util
 
+import android.content.res.Resources
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.ssafy.heritage.R
 import com.ssafy.heritage.util.DateFormatter.formatChatDate
+import kotlin.coroutines.coroutineContext
 
 @BindingAdapter("glide")
 fun setImage(v: ImageView, url: String) {
     if (!url.isNullOrEmpty() && url != "0" && url != "null") {
         Glide.with(v.context)
             .load(url)
+            .into(v)
+    } else{
+        Glide.with(v.context)
+            .load(R.drawable.image_ready)
             .into(v)
     }
 }
