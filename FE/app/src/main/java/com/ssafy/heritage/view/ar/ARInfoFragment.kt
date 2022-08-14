125a30da3e8eee9cff4e1fff82ecf9e5ca8fe9b8
foursoob <foursoob@naver.com>
2022-08-15 03:25:04 +0900

    [S07P12D102-254] FEAT: ar 순위 화면 구성

---
+++ FE/app/src/main/java/com/ssafy/heritage/view/ar/ARInfoFragment.kt
@@ -0,0 +1,18 @@
+package com.ssafy.heritage.view.ar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.heritage.R
+import com.ssafy.heritage.base.BaseFragment
+import com.ssafy.heritage.databinding.FragmentARInfoBinding
+
+private const val TAG = "ARInfoFragment___"
+
+class ARInfoFragment : BaseFragment<FragmentARInfoBinding>(R.layout.fragment_a_r_info) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
