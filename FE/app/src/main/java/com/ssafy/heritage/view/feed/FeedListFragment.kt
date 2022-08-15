2703001fd3732fde56102aca814773603d36fff7
foursoob <foursoob@naver.com>
2022-08-16 03:11:35 +0900

    [] UPDATE: feed 수정

--- FE/app/src/main/java/com/ssafy/heritage/view/feed/FeedListFragment.kt
+++ FE/app/src/main/java/com/ssafy/heritage/view/feed/FeedListFragment.kt
@@ -58,20 +58,20 @@ class FeedListFragment :
 //                    val newList = dataList.filter { it.userSeq == userSeq }
 //                    feedAdapter.submitList(newList)
                     feedViewModel.getMyFeedList()
-                    strokeFeedAll.visibility = View.GONE
-                    strokeFeedMy.visibility = View.VISIBLE
-                    tvFeedAll.setTextColor(Color.parseColor("#8e8e93"))
-                    tvFeedMy.setTextColor(Color.parseColor("@color/main_color"))
+//                    strokeFeedAll.visibility = View.GONE
+//                    strokeFeedMy.visibility = View.VISIBLE
+//                    tvFeedAll.setTextColor(Color.parseColor("#8e8e93"))
+//                    tvFeedMy.setTextColor(Color.parseColor("@color/main_color"))
                     Log.d(TAG, "setChip: 나의 피드")
                 }
                 else -> {
 //                    val newList = dataList.filter { true }
 //                    feedAdapter.submitList(newList)
                     feedViewModel.getFeedListAll()
-                    strokeFeedAll.visibility = View.VISIBLE
-                    strokeFeedMy.visibility = View.GONE
-                    tvFeedAll.setTextColor(Color.parseColor("@color/main_color"))
-                    tvFeedMy.setTextColor(Color.parseColor("#8e8e93"))
+//                    strokeFeedAll.visibility = View.VISIBLE
+//                    strokeFeedMy.visibility = View.GONE
+//                    tvFeedAll.setTextColor(Color.parseColor("@color/main_color"))
+//                    tvFeedMy.setTextColor(Color.parseColor("#8e8e93"))
                     Log.d(TAG, "setChip: 모두의 피드")
                 }
             }
