351873b62474fac2be828d533d2058936331b878
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-08-16 03:44:36 +0900

    [S07P12D102-295] UPDATE : 문화유산, 모임, 피드 UI 수정

--- FE/app/src/main/java/com/ssafy/heritage/view/group/GroupListFragment.kt
+++ FE/app/src/main/java/com/ssafy/heritage/view/group/GroupListFragment.kt
@@ -1,91 +1,54 @@
 package com.ssafy.heritage.view.group
 
-import android.util.Log
-import android.view.animation.OvershootInterpolator
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import com.ssafy.heritage.ApplicationClass
-import com.ssafy.heritage.R
-import com.ssafy.heritage.adpter.GroupListAdapter
-import com.ssafy.heritage.adpter.OnItemClickListener
+import androidx.viewpager.widget.ViewPager
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
 import com.ssafy.heritage.base.BaseFragment
-import com.ssafy.heritage.data.remote.response.GroupListResponse
 import com.ssafy.heritage.databinding.FragmentGroupListBinding
-import com.ssafy.heritage.view.dialog.SelectCategoryDialog
-import com.ssafy.heritage.viewmodel.GroupViewModel
-import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter
+
 
 private const val TAG = "GroupListFragment___"
 
 class GroupListFragment :
-    BaseFragment<FragmentGroupListBinding>(R.layout.fragment_group_list), OnItemClickListener {
-
-    private lateinit var groupListAdapter: GroupListAdapter
-    private val groupViewModel by activityViewModels<GroupViewModel>()
-    val userSeq: Int = ApplicationClass.sharedPreferencesUtil.getUser()
-
-    private val alphaInAnimationAdapter: ScaleInAnimationAdapter by lazy {
-        ScaleInAnimationAdapter(groupListAdapter).apply {
-            setDuration(300)
-            setInterpolator(OvershootInterpolator())
-            setFirstOnly(false)
-        }
-    }
+    BaseFragment<FragmentGroupListBinding>(com.ssafy.heritage.R.layout.fragment_group_list) {
 
     override fun init() {
 
-        groupViewModel.getGroupList()
         initAdapter()
-        initObserver()
-        initClickListener()
+
     }
 
-    private fun initAdapter() {
-        groupListAdapter = GroupListAdapter(this)
-        binding.recyclerviewGroupList.adapter = alphaInAnimationAdapter
-    }
+    private fun initAdapter() = with(binding) {
 
-    private fun initObserver() {
-        groupViewModel.groupList.observe(viewLifecycleOwner) {
-            // 진행
-            var list = mutableListOf<GroupListResponse>()
-            for (i in it) {
-                if (i.groupStatus == 'R') {
-                    list.add(i)
-                }
-            }
-            groupListAdapter.submitList(list)
-        }
-    }
-
-    private fun initClickListener() {
-        binding.apply {
-            fabCreateGroup.setOnClickListener {
-                findNavController().navigate(R.id.action_groupListFragment_to_groupCreateFragment)
-            }
-            btnMyGroup.setOnClickListener {
-                findNavController().navigate(R.id.action_groupListFragment_to_myGroupListFragment)
-            }
-            btnBack.setOnClickListener {
-                findNavController().popBackStack()
-            }
-            btnSort.setOnClickListener {
-                val selectCategoryDialog = SelectCategoryDialog()
-                selectCategoryDialog.show(childFragmentManager, "selectCategoryDialog")
-                selectCategoryDialog.adapter = groupListAdapter
-                selectCategoryDialog.list =
-                    groupViewModel.groupList.value!!.filter { it.groupStatus == 'R' } as MutableList<GroupListResponse>
-            }
-        }
-    }
-
-    override fun onItemClick(position: Int) {
-        Log.d(TAG, "onItemClick: ${position}")
-        val action = GroupListFragmentDirections.actionGroupListFragmentToGroupInfoFragment(
-            groupListAdapter.getItem(position)
+        val adapter = FragmentPagerItemAdapter(
+            childFragmentManager, FragmentPagerItems.with(requireContext())
+                .add("전체 모임", GroupListAllFragment()::class.java)
+                .add("나의 모임", MyGroupListFragment()::class.java)
+                .create()
         )
-        findNavController().navigate(action)
-    }
 
+        viewpager.adapter = adapter
+        viewpagertab.setViewPager(viewpager)
+
+//        viewpagertab.setOnTabClickListener {
+//            binding.toolbar.menu.getItem(0).isVisible = it == 0
+//        }
+
+        viewpagertab.setOnPageChangeListener(object :ViewPager.OnPageChangeListener{
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                binding.toolbar.menu.getItem(0).isVisible = position == 0
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+        })
+    }
 }
